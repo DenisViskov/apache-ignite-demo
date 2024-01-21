40486aa0627b80e6352829e32f49bd5b5e1fca2a
denis <denisviskov16@gmail.com>
2024-01-21 15:01:02 +0300

    Initial commit

---
+++ src/main/java/com/example/apacheignitedemo/ApacheIgniteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apacheignitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheIgniteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheIgniteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apacheignitedemo/ApacheIgniteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apacheignitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheIgniteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
